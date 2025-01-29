c356343f683d4c24f572fe1f08112e050b5500e5
Naeel <tazet@narod.ru>
2025-01-29 19:02:28 +0300

    logged

--- cmd/task2/chain.go
+++ cmd/task2/chain.go
@@ -1,16 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type chained struct {
 	ch chan int
 }
+
+var t time.Time
 
 func (s chained) filtering(threshold int) chained {
 	chacha := chained{ch: make(chan int)}
 	go func() {
 		defer close(chacha.ch)
 		for value := range s.ch {
+			fmt.Printf("Filtering %d %v\n", value, time.Since(t).Microseconds())
 			if value >= threshold {
 				chacha.ch <- value
 			}
@@ -24,6 +30,7 @@ func (s chained) increasing(percent int) chained {
 		defer close(chacha.ch)
 		for value := range s.ch {
 			chacha.ch <- value * (100 + percent) / 100
+			fmt.Printf("Increasing %d %v\n", value, time.Since(t).Microseconds())
 		}
 	}()
 	return chacha
@@ -34,6 +41,7 @@ func (s chained) summator() chained {
 		defer close(chacha.ch)
 		sum := 0
 		for value := range s.ch {
+			fmt.Printf("Summator %d %v\n", value, time.Since(t).Microseconds())
 			sum += value
 		}
 		chacha.ch <- sum
@@ -45,7 +53,9 @@ func (s chained) multip(mul float64) chained {
 	chacha := chained{ch: make(chan int)}
 	go func() {
 		defer close(chacha.ch)
-		chacha.ch <- int(mul * float64(<-s.ch))
+		value := int(mul * float64(<-s.ch))
+		chacha.ch <- value
+		fmt.Printf("multip %d %v\n", value, time.Since(t).Microseconds())
 		// for value := range s.ch {
 		// 	chacha.ch <- int(mul * float64(value))
 		// }
@@ -58,6 +68,8 @@ func main() {
 	threshold := 30
 	percent := 10
 	mul := 0.3
+
+	t = time.Now()
 
 	s := chained{ch: make(chan int)}
 
